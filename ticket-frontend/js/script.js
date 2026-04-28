802e2bc3f8cc3adc3327d9700a6fa22e67927bd6
kiengkvn2305 <trungkienngt2305@gmail.com>
2026-04-28 23:53:11 +0700

    update

---
+++ ticket-frontend/js/script.js
@@ -0,0 +1,28 @@
+function openPopup() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("popup").style.display = "none";
+}
+
+function toggleMenu(event) {
+  event.stopPropagation();
+
+  const menu = document.getElementById("menu");
+
+  if (!menu) {
+    console.error("Không tìm thấy #menu");
+    return;
+  }
+
+  menu.classList.toggle("show");
+}
+
+// click ngoài → đóng
+window.onclick = function () {
+  const menu = document.getElementById("menu");
+  if (menu) menu.classList.remove("show");
+};
